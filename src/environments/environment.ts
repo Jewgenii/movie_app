9c59ba4d5c0c1c6e1fd58c09ea7c20991b2f9d67
Ievhenii Ierokhin <jewgenijerochin@gmail.com>
2024-07-26 00:31:24 +0300

    expose credentials

---
+++ src/environments/environment.ts
@@ -0,0 +1,6 @@
+export const environment = {
+  "userName": "ievhenii",
+  "password": "q739vqFafJd3R?z",
+  "apiKey": "f5642906ca46c116a464edfb0723735f",
+  "apiAuthToken": "eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiJmNTY0MjkwNmNhNDZjMTE2YTQ2NGVkZmIwNzIzNzM1ZiIsIm5iZiI6MTcyMDE4NTUxMi40NzY0MDgsInN1YiI6IjY2ODQ1YzA4NWVlYzY3ZTg3ZDgxNjUyMiIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.YMoEuGhUIy8ltuvoG74Xyg4YXIdoK8qQFpk5dtOoJw8"
+};
